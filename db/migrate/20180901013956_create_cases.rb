0ffeae4e5979fe5af6fd509902d9e2a3cd7c1773
BrittanyTinnin <bntinnin7@gmail.com>
2018-09-01 16:04:50 -0700

    updated app_controller

--- db/migrate/20180901013956_create_cases.rb
+++ db/migrate/20180901013956_create_cases.rb
@@ -1,7 +1,8 @@
 class CreateCases < ActiveRecord::Migration
   def change
     create_table :cases do |t|
-
+      t.string :content
+      t.integer :user_id
       t.timestamps null: false
     end
   end
