2d0b46bc2057284df760c26051c4d9863eba967b
BrittanyTinnin <bntinnin7@gmail.com>
2018-09-02 17:06:19 -0700

    check code for functionality

--- app/controllers/cases_controller.rb
+++ app/controllers/cases_controller.rb
@@ -2,36 +2,38 @@ class CasesController < ApplicationController
 
   # GET: /cases
   get "/cases" do
-    erb :"/cases/index.html"
+    @user = current_user
+    @files = Case.all
+    erb :"cases/index"
   end
 
-  # GET: /cases/new
-  get "/cases/new" do
-    erb :"/cases/new.html"
-  end
+#   # GET: /cases/new
+#   get "/cases/new" do
+#     erb :"/cases/new.html"
+#   end
 
-  # POST: /cases
-  post "/cases" do
-    redirect "/cases"
-  end
+#   # POST: /cases
+#   post "/cases" do
+#     redirect "/cases"
+#   end
 
-  # GET: /cases/5
-  get "/cases/:id" do
-    erb :"/cases/show.html"
-  end
+#   # GET: /cases/5
+#   get "/cases/:id" do
+#     erb :"/cases/show.html"
+#   end
 
-  # GET: /cases/5/edit
-  get "/cases/:id/edit" do
-    erb :"/cases/edit.html"
-  end
+#   # GET: /cases/5/edit
+#   get "/cases/:id/edit" do
+#     erb :"/cases/edit.html"
+#   end
 
-  # PATCH: /cases/5
-  patch "/cases/:id" do
-    redirect "/cases/:id"
-  end
+#   # PATCH: /cases/5
+#   patch "/cases/:id" do
+#     redirect "/cases/:id"
+#   end
 
-  # DELETE: /cases/5/delete
-  delete "/cases/:id/delete" do
-    redirect "/cases"
-  end
+#   # DELETE: /cases/5/delete
+#   delete "/cases/:id/delete" do
+#     redirect "/cases"
+#   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,5 +2,7 @@ User.create(username: "jody", email: "jodyrocks@me.com", password: "password34")
 User.create(username: "clue", email: "cluerocks@me.com", password: "password98")
 User.create(username: "booboo", email: "bamboogo@me.com", password: "sometypewood")
 User.create(username: "girlsrule", email: "girlsrule@me.com", password: "girlalwaysrule")
+User.create(username: "10", email: "ten@me.com", password: "passten")
 
-Case.create(content: "attempts to get a job at a local fast food restaurant called the Krusty Krab")
+
+Case.create(content: "SpongeBob meets and befriends a squirrel named Sandy Cheeks")
